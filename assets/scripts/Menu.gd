ecc4eeb1d95a1949f9a8555a23480b6669508b11
MrMatras <169089986+MrMatras@users.noreply.github.com>
2024-09-22 16:46:58 +0300

    assets files add, project.godot and export config

---
+++ assets/scripts/Menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://assets/scenes/game.tscn")
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://assets/scenes/options.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ assets/scripts/Options.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_back_to_menu_pressed():
+	get_tree().change_scene_to_file("res://assets/scenes/menu.tscn")
+
+var master_bus = AudioServer.get_bus_index("Menu") 
+
+func _on_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
