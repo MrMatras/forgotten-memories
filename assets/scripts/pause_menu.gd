2954c2a03bf0116b08c35c03e511701a20dee330
MrMatras <169089986+MrMatras@users.noreply.github.com>
2024-10-13 17:50:46 +0300

    small additions to 3d scene and player movement

--- assets/scripts/pause_menu.gd
+++ assets/scripts/pause_menu.gd
@@ -1,27 +1,10 @@
 extends Control
 
-func _ready():
-	$AnimationPlayer.play("RESET")
-
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
-
-func pause():
-	get_tree().paused = true
-	$AnimationPlayer.play("blur")
-
-func testEscape():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
-		resume()
-
-func _on_button_resume_pressed():
-	resume()
-
-func _on_button_quit_pressed():
-	get_tree().change_scene_to_file("res://assets/scenes/menu.tscn")
-
 func _process(delta):
-	testEscape()
+	if Input.is_action_just_pressed("escape"):
+		pause_or_unpause()
+
+func pause_or_unpause():
+	var is_paused = get_tree().paused
+	$PanelContainer/button_resume.visible = is_paused
+	get_tree().paused = !is_paused
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,26 +1,27 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var t_bob = 0.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+@onready var camera = $head/camera                         
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Добавляем гравитацию
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
+	# Обрабатываем прыжок
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Получаем направление ввода и обрабатываем движение
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -28,4 +29,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	# Покачивание камеры
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin.y = _headbob(t_bob).y  # Применяем только Y-координату покачивания
+	
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	return Vector3(0, sin(time * BOB_FREQ) * BOB_AMP, 0)
